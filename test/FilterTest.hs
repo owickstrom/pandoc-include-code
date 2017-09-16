ffb96d27311c3740e2325890261d472bd204c9e3
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-09-16 17:12:21 +0200

    Add more tests

--- test/FilterTest.hs
+++ test/FilterTest.hs
@@ -1,24 +1,61 @@
+{-# LANGUAGE TupleSections #-}
+
 module FilterTest where
 
-import Test.Tasty
-import Test.Tasty.HUnit
+import           Test.Tasty
+import           Test.Tasty.HUnit
 
-import Text.Pandoc.Filter.IncludeCode
-import Text.Pandoc.JSON
+import qualified Text.Pandoc.Filter.IncludeCode as Filter
+import           Text.Pandoc.JSON
+
+(@@?=) a b = a >>= (@?= b)
+
+type Formatted = Bool
+
+includeCode ::
+     Format
+  -> String
+  -> Maybe String
+  -> (Maybe Word, Maybe Word)
+  -> Formatted
+  -> IO Block
+includeCode fmt fixtureName snippet (startLine, endLine) formatted =
+  Filter.includeCode
+    (Just fmt)
+    (CodeBlock
+       ( ""
+       , []
+       , mconcat
+           [ [("include", "test/fixtures/" ++ fixtureName)]
+           , maybe [] ((: []) . ("snippet", )) snippet
+           , maybe [] ((: []) . ("startLine", ) . show) startLine
+           , maybe [] ((: []) . ("endLine", ) . show) endLine
+           , [("formatted", "true") | formatted]
+           ])
+       "")
+
+includeCodeForHtml ::
+     String -> Maybe String -> (Maybe Word, Maybe Word) -> Formatted -> IO Block
+includeCodeForHtml = includeCode (Format "html5")
+
+noRange = (Nothing, Nothing)
+
+codeBlock :: String -> Block
+codeBlock = CodeBlock ("", [], [])
 
 tests =
   testGroup
-    "Filter"
-    [ testCase "includes snippet" $ do
-        out <-
-          includeCode
-            (Just (Format "html5"))
-            (CodeBlock
-               ( ""
-               , []
-               , [ ("include", "test/fixtures/foo-snippet.txt")
-                 , ("snippet", "foo")
-                 ])
-               "")
-        out @?= CodeBlock ("", [], []) "foo\n"
+    "Text.Pandoc.Filter.IncludeCode"
+    [ testCase "includes snippet" $
+      includeCodeForHtml "foo-snippet.txt" (Just "foo") noRange False @@?=
+      codeBlock "foo\n"
+    , testCase "includes snippet within start and end line" $
+      includeCodeForHtml "foo-snippet.txt" (Just "foo") (Just 1, Just 3) False @@?=
+      codeBlock "foo\n"
+    , testCase "excludes snippet outside start and end line" $
+      includeCodeForHtml "foo-snippet.txt" (Just "foo") (Just 2, Just 3) False @@?=
+      codeBlock ""
+    , testCase "includes only lines between start and end line" $
+      includeCodeForHtml "some-text.txt" Nothing (Just 2, Just 3) False @@?=
+      codeBlock "world\nthis\n"
     ]
