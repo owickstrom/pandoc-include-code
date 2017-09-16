276e0e6b65b3771257e14320839494a76a80825c
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-09-16 18:16:13 +0200

    Use monad transformer and clean up the composition

--- src/Text/Pandoc/Filter/IncludeCode.hs
+++ src/Text/Pandoc/Filter/IncludeCode.hs
@@ -1,55 +1,84 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns   #-}
+{-# LANGUAGE RecordWildCards  #-}
 
 module Text.Pandoc.Filter.IncludeCode where
 
-import qualified Data.Map              as Map
-
-import           Data.Function         ((&))
-import           Data.List             (isInfixOf)
+import           Control.Monad.Reader
+import           Data.Function        ((&))
+import           Data.HashMap.Strict  (HashMap)
+import qualified Data.HashMap.Strict  as HM
+import           Data.List            (isInfixOf)
 import           Text.Pandoc.JSON
-import           Text.Read
+import           Text.Read            (readMaybe)
 
-getRange :: Map.Map String String -> Maybe (Int, Int)
-getRange attrs = do
-  start <- Map.lookup "startLine" attrs >>= readMaybe
-  end <- Map.lookup "endLine" attrs >>= readMaybe
-  if start <= end
-    then return (start, end)
-    else Nothing
+data InclusionSpec = InclusionSpec
+  { include :: FilePath
+  , snippet :: Maybe String
+  , range   :: Maybe (Int, Int)
+  }
 
-withinLines :: Maybe (Int, Int) -> String -> String
-withinLines range =
-  case range of
+type Inclusion = ReaderT InclusionSpec IO
+
+runInclusion :: InclusionSpec -> Inclusion a -> IO a
+runInclusion = flip runReaderT
+
+parseInclusion :: HashMap String String -> Maybe InclusionSpec
+parseInclusion attrs = do
+  include <- HM.lookup "include" attrs
+  return InclusionSpec {..}
+  where
+    snippet = HM.lookup "snippet" attrs
+    range = do
+      start <- HM.lookup "startLine" attrs >>= readMaybe
+      end <- HM.lookup "endLine" attrs >>= readMaybe
+      if start <= end
+        then return (start, end)
+        else Nothing
+
+readIncluded :: Inclusion String
+readIncluded = do
+  path <- asks include
+  lift (readFile path)
+
+filterLineRange :: String -> Inclusion String
+filterLineRange contents = do
+  r <- asks range
+  case r of
     Just (start, end) ->
-      unlines . take (end - startIndex) . drop startIndex . lines
+      return
+        (unlines (take (end - startIndex) (drop startIndex (lines contents))))
       where startIndex = pred start
-    Nothing -> id
+    Nothing -> return contents
 
-onlySnippet :: Map.Map String String -> String -> String
-onlySnippet attrs =
-  case Map.lookup "snippet" attrs of
+onlySnippet :: String -> Inclusion String
+onlySnippet contents = do
+  s <- asks snippet
+  case s of
     Just name ->
-      unlines .
-      takeWhile (not . isSnippetEnd) .
-      drop 1 . dropWhile (not . isSnippetStart) . lines
+      contents & lines & dropWhile (not . isSnippetStart) &
+      takeWhile (not . isSnippetEnd) &
+      unlines &
+      drop 1 &
+      return
       where isSnippetTag tag line =
               (tag ++ " snippet " ++ name) `isInfixOf` line
             isSnippetStart = isSnippetTag "start"
             isSnippetEnd = isSnippetTag "end"
-    Nothing -> id
+    Nothing -> return contents
+
+filterAttributes :: [(String, String)] -> [(String, String)]
+filterAttributes = filter nonFilterAttribute
+  where
+    nonFilterAttribute (key, _) = key `elem` attributeNames
+    attributeNames = ["include", "startLine", "endLine", "snippet"]
 
 includeCode :: Maybe Format -> Block -> IO Block
-includeCode (Just fmt) cb@(CodeBlock (id', classes, attrs) _) = do
-  let attrs' = Map.fromList attrs
-  case Map.lookup "include" attrs' of
-    Just f -> do
-      fileContents <- readFile f
-      let filteredLines =
-            fileContents & withinLines (getRange attrs') & onlySnippet attrs'
-          paramNames =
-            ["include", "startLine", "endLine", "snippet"]
-          filteredAttrs = foldl (flip Map.delete) attrs' paramNames
-          classes' = unwords classes
-      return (CodeBlock (id', classes, Map.toList filteredAttrs) filteredLines)
+includeCode _ cb@(CodeBlock (id', classes, attrs) _) =
+  case parseInclusion (HM.fromList attrs) of
+    Just i -> do
+      contents <-
+        runInclusion i $ readIncluded >>= filterLineRange >>= onlySnippet
+      return (CodeBlock (id', classes, filterAttributes attrs) contents)
     Nothing -> return cb
 includeCode _ x = return x
